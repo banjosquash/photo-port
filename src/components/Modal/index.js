7900f1ea7db7fa45e4506edd4df789ef495cb4e1
banjosquash <banjosquash@gmail.com>
2022-11-21 13:15:51 -0500

    pushing to gh pages

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-function Modal({currentPhoto}) {
+const Modal = ({ onClose, currentPhoto }) => {
    
     const {name, category, description, index} = currentPhoto;
     return (
@@ -12,9 +12,7 @@ function Modal({currentPhoto}) {
                 <p>
                     {description}
                 </p>
-                <button type="button">
-                    Close this modal
-                </button>
+                <button onClick={onClose} type="button">Close this modal</button>
             </div>
         </div>
     );
